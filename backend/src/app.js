9559454c111c5e332b8357f232d074bb56e16b77
Aditya Pandey <adityapandey1609@gmail.com>
2026-02-08 08:10:24 +0530

    Minor Edit

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
 	cors({
-		origin: ["https://jamuna-sense.vercel.app", "https://jamuna-sense-dashboard.vercel.app"],
+		origin: ["https://jamuna-sense.vercel.app", "https://jamuna-sense-dashboard.vercel.app", "http://localhost:5173"],
 		credentials: true,
 	}),
 );
