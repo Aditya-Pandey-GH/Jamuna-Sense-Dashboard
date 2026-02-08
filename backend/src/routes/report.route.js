76eb58e7c5eaf6363d558797479d6c8d7fed04f3
Aditya Pandey <adityapandey1609@gmail.com>
2026-02-08 08:05:35 +0530

    Added support for fetching data

--- backend/src/routes/report.route.js
+++ backend/src/routes/report.route.js
@@ -3,11 +3,18 @@ import { Report } from "../models/Report.model.js";
 
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+	try {
+		const reports = await Report.find();
+		res.status(200).json({ success: true, reports });
+	} catch (err) {
+		res.status(400).json({ success: false, message: err.message });
+	}
+});
+
 router.post("/", async (req, res) => {
 	try {
 		const report = await Report.create(req.body);
-		console.log(report);
-		// res.send("LOL LMAO");
 		res.status(201).json({ success: true, report });
 	} catch (err) {
 		res.status(400).json({ success: false, message: err.message });
